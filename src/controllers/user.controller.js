2474a97ad7bb8d4aa365a4308f2c0f669b26f50a
Juan Camilo <jcjdlt@hotmail.com>
2023-03-06 13:36:35 -0500

    Finalizacion panel admin

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,13 +14,28 @@ export const renderUserAdmin = async (req, res, next) => {
   const [prices] = await pool.query("SELECT sum(price) price FROM appointment a JOIN nails n ON a.id_nails = n.id WHERE a.id_state = 2");
   const [users] = await pool.query("SELECT count(u.id) nUsers FROM users u WHERE u.id_rol = 2");
 
-  const [rows] = await pool.query("SELECT MONTH(date) as mes, COUNT(*) as cantidad FROM appointment a WHERE a.id_state = 2 AND YEAR(a.date) = YEAR(CURDATE()) GROUP BY MONTH(a.date)");
+  const [rows] = await pool.query("SELECT MONTHNAME(date) as name, COUNT(*) as cantidad FROM appointment a WHERE a.id_state = 2 AND YEAR(a.date) = YEAR(CURDATE()) GROUP BY MONTHNAME(a.date)");
+  const [rows2] = await pool.query("SELECT MONTHNAME(date) as name,sum(n.price) as cantidad FROM appointment a join nails n on a.id_nails = n.id WHERE a.id_state = 2 AND YEAR(a.date) = YEAR(CURDATE()) GROUP BY MONTHNAME(a.date)");
+  const [rows3] = await pool.query("SELECT n.name as name, COUNT(*) as cantidad FROM appointment a join nails n on a.id_nails = n.id WHERE a.id_state = 2 AND YEAR(a.date) = YEAR(CURDATE()) GROUP BY n.name");
+  const [rows4] = await pool.query("SELECT n.name, sum(n.price) as cantidad FROM appointment a join nails n on a.id_nails = n.id WHERE a.id_state = 2 AND YEAR(a.date) = YEAR(CURDATE()) GROUP BY n.name");
   
-  const data = JSON.stringify(rows);
+  const data1 = convertJSON(rows);
+  const data2 = convertJSON(rows2);
+  const data3 = convertJSON(rows3);
+  const data4 = convertJSON(rows4);
+
   res.render("admin", {
     count: acounts[0].count,
     price: prices[0].price,
     nUsers: users[0].nUsers,
-    rows: data
+    rows1: data1,
+    rows2: data2,
+    rows3: data3,
+    rows4: data4,
   });
 };
+
+export const convertJSON = (rows) => {
+  const data = JSON.stringify(rows);
+  return data;
+}
